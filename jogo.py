6f8c49b958ea2b0f2147f729580773eeff8d7ad9
Ivansallas <Kiss_ivan@hotmail.com>
2026-06-25 13:26:59 -0300

    oko

--- jogo.py
+++ jogo.py
@@ -1,27 +1,16 @@
-import random
+#Trabalho de introdução a programação
+#Parte1
+
 print("********************************")
 print("Bem vindo ao jogo de Adivinhação")
 print("********************************")
 
-numerosecreto = random.randrange(1, 51)
-totaldetentativas = 10
+numerosecreto = 40
 
+chute = input("Digite o seu número: ")
+print("Você digitou: ", chute )
 
-
-while(totaldetentativas > 0):
-    print("Você tem ", totaldetentativas, " tentativas")   
-    
-    chute = input("Digite o seu número: ")
-    print("Você digitou: ", chute )
-
-    chuteNumerico = int(chute)
-    
-
-    if(totaldetentativas == 0):
-        print("Você não tem mais tentativas. Fim do jogo.")
-        break
-
-
+chuteNumerico = int(chute)
 
 acertou = chuteNumerico == numerosecreto
 maior = chuteNumerico > numerosecreto
@@ -29,14 +18,9 @@ menor = chuteNumerico < numerosecreto
 
 
 #se voce digitar qualquer numero vou verificar se acertou ou errou
-if(acertou):
-    print("Parabéns! Você acertou! Fim do jogo")
-   
+if(numerosecreto == chuteNumerico):
+    print("Você acertou!")
 else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    print("Você errou!")
 
-totaldetentativas = totaldetentativas - 1
 print("Fim do jogo")

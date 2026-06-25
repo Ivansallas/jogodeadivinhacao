0211ad7b02c0bf90d7e523b1c7ab3e28f014e8d9
Ivansallas <Kiss_ivan@hotmail.com>
2026-06-25 16:11:28 -0300

    up

--- jogo.py
+++ jogo.py
@@ -1,7 +1,8 @@
-#Trabalho de introdução a programação
-#Parte2
+# Trabalho de introdução a programação
+# Parte3
 
 import random
+
 print("********************************")
 print("Bem vindo ao jogo de Adivinhação")
 print("********************************")
@@ -14,39 +15,34 @@ print("Qual nível de dificuldade você deseja?")
 print("(1) Fácil (2) Médio (3) Difícil")
 nivel = int(input("Defina o nível: "))
 
-if(nivel == 1):
+if nivel == 1:
     totaldetentativas = 20
-elif(nivel == 2):
+elif nivel == 2:
     totaldetentativas = 10
-elif(nivel == 3):
+elif nivel == 3:
     totaldetentativas = 5
 
 
+while totaldetentativas > 0:
+    print("Você tem ", totaldetentativas, " tentativas")
 
-while(totaldetentativas > 0):
-    print("Você tem ", totaldetentativas, " tentativas")   
-    
     chute = input("Digite o seu número: ")
-    print("Você digitou: ", chute )
+    print("Você digitou: ", chute)
 
     chuteNumerico = int(chute)
-    
 
-    if(totaldetentativas == 0):
+    if totaldetentativas == 0:
         print("Você não tem mais tentativas. Fim do jogo.")
         break
-
-
 
     acertou = chuteNumerico == numerosecreto
     maior = chuteNumerico > numerosecreto
     menor = chuteNumerico < numerosecreto
 
+    # se voce digitar qualquer numero vou verificar se acertou ou errou
+    if acertou:
+        print(f"Parabéns! Você acertou! um total de {pontos} pontos")
 
-    #se voce digitar qualquer numero vou verificar se acertou ou errou
-    if(acertou):
-        print("Parabéns! Você acertou! Fim do jogo")
-    
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto.")

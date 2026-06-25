4d629d82821b10c2b105740b260faa6d98a9a1f0
Prof: Ivan <79423340+Ivansallas@users.noreply.github.com>
2026-06-25 15:21:50 +0000

    save

--- jogo.py
+++ jogo.py
@@ -1,13 +1,27 @@
+import random
 print("********************************")
 print("Bem vindo ao jogo de Adivinhação")
 print("********************************")
 
-numerosecreto = 40
+numerosecreto = random.randrange(1, 51)
+totaldetentativas = 10
 
-chute = input("Digite o seu número: ")
-print("Você digitou: ", chute )
 
-chuteNumerico = int(chute)
+
+while(totaldetentativas > 0):
+    print("Você tem ", totaldetentativas, " tentativas")   
+    
+    chute = input("Digite o seu número: ")
+    print("Você digitou: ", chute )
+
+    chuteNumerico = int(chute)
+    
+
+    if(totaldetentativas == 0):
+        print("Você não tem mais tentativas. Fim do jogo.")
+        break
+
+
 
 acertou = chuteNumerico == numerosecreto
 maior = chuteNumerico > numerosecreto
@@ -15,9 +29,14 @@ menor = chuteNumerico < numerosecreto
 
 
 #se voce digitar qualquer numero vou verificar se acertou ou errou
-if(numerosecreto == chuteNumerico):
-    print("Você acertou!")
+if(acertou):
+    print("Parabéns! Você acertou! Fim do jogo")
+   
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o número secreto.")
 
+totaldetentativas = totaldetentativas - 1
 print("Fim do jogo")

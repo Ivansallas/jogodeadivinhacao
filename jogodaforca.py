b84b681a0b4f7a9f55c139fbad6e7bdc40470de4
Prof: Ivan <79423340+Ivansallas@users.noreply.github.com>
2026-07-30 15:15:38 +0000

    adiciona o jogo da forca

---
+++ jogodaforca.py
@@ -0,0 +1,24 @@
+#Jogo da forca
+print("********************************")
+print("Bem vindo ao jogo da Foca")
+print("********************************")
+
+palavrasecreta = "forte"
+letrasacertadas = ["_", "_", "_", "_", "_", "_"]
+
+enforcou = False
+acertou = False
+
+while(not enforcou and not acertou):
+    chute = input("Digite uma letra? ")
+    chute = chute.strip()
+
+    index = 0
+    for letra in palavrasecreta:
+        if(chute.upper() == letra.upper()):
+            print("Encontrei a letra {} na posição {}".format(letra, index))
+        index = index + 1
+
+    print("jogando")    
+
+print("Fim do jogo")

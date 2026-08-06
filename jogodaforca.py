1efe1189537da59deb129b1132b4b4232910d8fd
Ivansallas <kiss_ivan@hotmail.com>
2026-08-05 22:44:06 -0300

    atualiza a palavra secreta para 'uva' e melhora a lógica de verificação de letras

--- jogodaforca.py
+++ jogodaforca.py
@@ -3,7 +3,7 @@ print("********************************")
 print("Bem vindo ao jogo da Foca")
 print("********************************")
 
-palavrasecreta = "forte"
+palavrasecreta = "uva".upper()
 letrasacertadas = ["_"] * len(palavrasecreta)
 total_tentativas = len(palavrasecreta)
 
@@ -11,22 +11,31 @@ enforcou = False
 acertou = False
 tentativas = 0
 
+print("A palavra secreta tem {} letras".format(len(palavrasecreta)))
+print(letrasacertadas)
 while(not enforcou and not acertou and tentativas < total_tentativas):
     chute = input("Digite uma letra? ")
-    chute = chute.strip()
-
-    index = 0
-    for letra in palavrasecreta:
-        if(chute.upper() == letra.upper()):
-            letrasacertadas[index] = letra
-            print("Encontrei a letra {} na posição {}".format(letra, index))
-        index = index + 1
-
-    tentativas += 1
+    chute = chute.strip().upper()
+    
+    if (chute in palavrasecreta):
+        index = 0
+        for letra in palavrasecreta:
+            if(chute == letra):
+                letrasacertadas[index] = letra
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+    else:
+        tentativas += 1
+        
+    enforcou = tentativas == total_tentativas
+    acertou = "_" not in letrasacertadas
     print("Letras acertadas:", letrasacertadas)
     print("Tentativas usadas:", tentativas)
-
-    if "_" not in letrasacertadas:
-        acertou = True
+    
+    # Verifica se o jogador ganhou ou perdeu
+    if (acertou):
+        print("Parabéns, você ganhou!")
+    elif (enforcou):
+        print("Você perdeu!")
 
 print("Fim do jogo")

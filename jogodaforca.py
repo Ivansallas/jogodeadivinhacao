87041ae0d01f9b7ed3746f1e2900049cf5022d5a
Ivansallas <kiss_ivan@hotmail.com>
2026-08-05 22:10:43 -0300

    corrige a inicialização da variável 'acertou' e ajusta a lógica do jogo da forca

--- jogodaforca.py
+++ jogodaforca.py
@@ -1,24 +1,32 @@
-#Jogo da forca
+# Jogo da forca
 print("********************************")
 print("Bem vindo ao jogo da Foca")
 print("********************************")
 
 palavrasecreta = "forte"
-letrasacertadas = ["_", "_", "_", "_", "_", "_"]
+letrasacertadas = ["_"] * len(palavrasecreta)
+total_tentativas = len(palavrasecreta)
 
 enforcou = False
-acertou = true
+acertou = False
+tentativas = 0
 
-while(not enforcou and not acertou):
+while(not enforcou and not acertou and tentativas < total_tentativas):
     chute = input("Digite uma letra? ")
     chute = chute.strip()
 
     index = 0
     for letra in palavrasecreta:
         if(chute.upper() == letra.upper()):
+            letrasacertadas[index] = letra
             print("Encontrei a letra {} na posição {}".format(letra, index))
         index = index + 1
 
-    print("jogando")    
+    tentativas += 1
+    print("Letras acertadas:", letrasacertadas)
+    print("Tentativas usadas:", tentativas)
+
+    if "_" not in letrasacertadas:
+        acertou = True
 
 print("Fim do jogo")

ca34ad1c39041ba7135b4eacded9e601582bf137
Ivansallas <kiss_ivan@hotmail.com>
2026-08-12 23:16:50 -0300

    Revert "adiciona lógica para selecionar palavra secreta aleatória e inclui arquivo de palavras"
    This reverts commit cb7375f4df4b044d5b90111ecc349e95f08a477f.

--- jogodaforca.py
+++ jogodaforca.py
@@ -1,20 +1,10 @@
-import random
-
 # Jogo da forca
 print("********************************")
 print("Bem vindo ao jogo da Foca")
 print("********************************")
 
-#Lendo arquivo de palavras
-arquivo = open("palavras.txt", "r")
-palavras = []
-for linha in arquivo:
-    palavras.append(linha.strip().upper())
-arquivo.close()
 
-numero = random.randrange(0, len(palavras))
-    
-palavrasecreta = palavras[numero].upper()
+palavrasecreta = "uva".upper()
 letrasacertadas = ["_"] * len(palavrasecreta)
 total_tentativas = len(palavrasecreta)
 
@@ -24,7 +14,6 @@ tentativas = 0
 
 print("A palavra secreta tem {} letras".format(len(palavrasecreta)))
 print(letrasacertadas)
-
 while(not enforcou and not acertou and tentativas < total_tentativas):
     chute = input("Digite uma letra? ")
     chute = chute.strip().upper()
